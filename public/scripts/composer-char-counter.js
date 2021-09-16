1a5e40ac6dd984e3dba0928b4b75706cfbb21d30
Douglas Young <dyoung4747@hotmail.com>
2021-09-16 02:02:20 +0000

    showing previous tweets

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+  $("#tweet-text").on('input', function(event) {
+    // capture input in textarea 
+    let $formInput = $(this);
+    // find closest form tag
+    let $forms = $formInput.closest('form');
+    console.log('forms', $forms);
+    // find id to change
+    let $textCount = $forms.find("#counter");
+    console.log('text count', $textCount)
+    // determine textarea input length
+    let textLength = $formInput.val().length;
+    console.log('text length', textLength)
+    // subtract text length from 140
+    let charLeft = 140 - textLength;
+    // display characters left on specified id (counter)
+    $textCount.html(charLeft);
+    
+    if (charLeft < 0) {
+      $textCount.css('color', 'red');
+    } else {
+      $textCount.css('color', 'black');
+    }
+  });
+    
+});
